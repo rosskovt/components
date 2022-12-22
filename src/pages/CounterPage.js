62613ea4e9d99d9cefbe12c739b859179baa327d
rosskovt <rostikk@gmail.com>
2022-12-22 20:37:37 +0200

    refactored to use a reducer for a state management

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,16 +1,127 @@
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import { useReducer } from "react";
+import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
+const ADD_VALUE_TO_COUNT = 'add-from-form';
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1,
+            }
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1,
+            }
+        case SET_VALUE_TO_ADD:
+            return {
+                ...state,
+                valueToAdd: action.payload,
+            }
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0,
+            }
+        default:
+            // throw new Error('unexpected action type: ' + action.type);
+            return state;
+    }
+
+    // if (action.type === INCREMENT_COUNT) {
+    //     return {
+    //         ...state,
+    //         count: state.count + 1,
+    //     }
+    // };
+    // if (action.type === DECREMENT_COUNT) {
+    //     return {
+    //         ...state,
+    //         count: state.count - 1,
+    //     }
+    // };
+    // if (action.type === SET_VALUE_TO_ADD) {
+    //     return {
+    //         ...state,
+    //         valueToAdd: action.payload
+    //     }
+    // };
+    // if (action.type === FORM_ADD) {
+    //     return {
+    //         ...state,
+    //         count: action.payload,
+    //         valueToAdd: 0
+    //     }
+    // }
+    // return state;
+};
 
 function CounterPage({ initialCount }) {
-    const { count, increment } = useCounter(initialCount);
+    // const [count, setCount] = useState(initialCount);
+    // const [valueToAdd, setValueToAdd] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    })
+    const increment = (counter) => {
+        // setCount((counter) => {
+        //     return counter + 1;
+        // });
+        dispatch({
+            type: INCREMENT_COUNT
+        });
+    };
+
+    const decrement = (counter) => {
+        // setCount((counter) => {
+        //     return counter - 1;
+        // });
+        dispatch({
+            type: DECREMENT_COUNT
+        });
+    };
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+        // setValueToAdd(value)
+        dispatch({
+            type: SET_VALUE_TO_ADD,
+            payload: value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // setCount(count + valueToAdd);
+        // setValueToAdd(0);
+        dispatch({
+            type: ADD_VALUE_TO_COUNT,
+        });
+    }
+
     return (
-        <div>
-            <h1>
-                Count is {count}
+        <Panel className="m-3">
+            <h1 className="text-lg">
+                Count is {state.count}
             </h1>
-            <Button onClick={() => increment(count)}>Increment</Button>
-        </div>
+            <div className="flex flex-row">
+                <Button onClick={() => increment(state.count)}>Increment</Button>
+                <Button onClick={() => decrement(state.count)}>Decrement</Button>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot</label>
+                <input value={state.valueToAdd || ''} onChange={handleChange} type="number" className="p-1 m-3 bg-gray-50 border border-gray-300"></input>
+                <Button>Add it</Button>
+            </form>
+
+        </Panel>
     )
 }
 
